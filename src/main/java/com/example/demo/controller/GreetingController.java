9361a63343d52362f1b2dcb6a468ac5fe0540320
Vadym <turokvadim2510@gmail.com>
2022-09-19 13:06:47 +0300

    first commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "greeting";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
+
+}
